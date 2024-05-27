0d888355df2af76b5393afbcc1a701417824259c
Azizbek Rustamov <rystamov2233@gmail.com>
2024-05-27 20:14:48 +0500

    js

---
+++ index.js
@@ -0,0 +1,8 @@
+let son = prompt("foydalanuchidan son oling");
+let taxminiyson = Math.floor(Math.random() * 100);``
+
+
+alert(son + taxminiyson);
+alert(son - taxminiyson);
+alert(son * taxminiyson);
+alert(son / taxminiyson);
